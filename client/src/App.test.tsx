97bc82bad71b59d339df199e776987e539ccfbd1
tungle251 <tungle251@gmail.com>
2022-11-12 22:23:22 +0700

    Script for Vercel deployment (#4)

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText('Books Market')
+  const linkElement = screen.getByText('Book Market')
   expect(linkElement).toBeInTheDocument()
 })
