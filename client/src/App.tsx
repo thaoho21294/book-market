81c2e6b96b9e258710afc2ca3e5de8736bb489d0
Tung Le <tungle251@gmail.com>
2022-11-12 19:51:56 +0700

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,7 @@ import About from 'scenes/about'
 import PageNotFound from 'components/page-not-found/PageNotFound'
 
 const router = createBrowserRouter(
+  let a = null;
   createRoutesFromElements(
     <Route>
       <Route path='/' element={<RootLayout />}>
