e82511013b3b1ed19460f7b77ac3f969b16af83d
Tung Le <tungle251@gmail.com>
2022-11-12 21:59:33 +0700

    test error

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
+  const a = 1;
   render(<App />)
   const linkElement = screen.getByText('Book Market')
   expect(linkElement).toBeInTheDocument()
