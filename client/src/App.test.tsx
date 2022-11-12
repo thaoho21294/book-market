f2430191b20e08d3a17ca8e097f0a520d90e9e22
Tung Le <tungle251@gmail.com>
2022-11-12 22:02:48 +0700

    fix eslintd

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-  const a = 1;
   render(<App />)
   const linkElement = screen.getByText('Book Market')
   expect(linkElement).toBeInTheDocument()
